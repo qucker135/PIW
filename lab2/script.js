22fe66bac293cd2b01f7d7c6d725594c4469d882
Bartosz Matysiak <qucker135@gmail.com>
2022-03-13 00:57:01 +0100

    Lab2 - add datetime (1.5) (ugly version)

--- lab2/script.js
+++ lab2/script.js
@@ -4,8 +4,28 @@ function dodajZadanie(){
 	let taskContent = document.getElementById("zadanie").value;
 	if(taskContent!==""){
 		let newElement = document.createElement("div");
-		newElement.innerHTML = taskContent;
-		newElement.onclick = () => {newElement.classList.toggle("greyed");};
+		newElement.innerHTML = taskContent + '<div class="date" style="display: none;">'+2022+'</div>';
+		newElement.onclick = () => {
+			newElement.classList.toggle("greyed");
+			for (let i = 0; i < newElement.children.length; i++) {
+			  console.log(newElement.children[i].tagName);
+			}
+			let today = new Date();
+			let dd = String(today.getDate()).padStart(2, '0');
+			let mm = String(today.getMonth() + 1).padStart(2, '0');
+			let yyyy = String(today.getFullYear());
+
+			let hh = String(today.getHours());
+			let ii = String(today.getMinutes());
+			let ss = String(today.getSeconds());
+			
+			newElement.children[0].innerHTML = dd + '/' + mm + '/' + yyyy + ' ' + hh + ':' + ii + ':' + ss;
+			if (newElement.children[0].style.display === "none") {
+                            newElement.children[0].style.display = "block";
+                        } else {
+                            newElement.children[0].style.display = "none";
+                        }
+		};
 		document.getElementById('list1').appendChild(newElement);		
 	}
 }
