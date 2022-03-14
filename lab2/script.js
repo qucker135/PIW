6e9306f2dd4c9faffde6e2bb052ee99e82b10b35
Bartosz Matysiak <qucker135@gmail.com>
2022-03-14 10:17:19 +0100

    Lab2 - convert to jquery and add modal (3.0)

--- lab2/script.js
+++ lab2/script.js
@@ -1,10 +1,15 @@
 'use strict'
 
-let trash = ""; 
+//let trash = ""; 
+let $trash = "";
+let $copiedElement = $("");
 
+let numberOfElements = 0; //TO DELETE???
+
+/*unused*/
 function usunElement(){
-	console.log(this + "usunelement");
-	console.log(this.parentNode);
+	//console.log(this + "usunelement");
+	//console.log(this.parentNode);
 	trash = this.parentNode.cloneNode(true);
 	this.parentNode.remove();
 }
@@ -12,37 +17,122 @@ function usunElement(){
 function dodajZadanie(){
 	let taskContent = document.getElementById("zadanie").value;
 	if(taskContent!==""){
-		let newElement = document.createElement("div");
-		newElement.innerHTML = taskContent + '<div class="date" style="display: none;">'+2022+'</div>';
-		newElement.innerHTML = newElement.innerHTML + '<button onclick="trash = parentNode.cloneNode(true); parentNode.remove();">x</button>'
-		newElement.onclick = () => {
-			newElement.classList.toggle("greyed");
-			for (let i = 0; i < newElement.children.length; i++) {
-			  console.log(newElement.children[i].tagName);
-			}
-			let today = new Date();
-			let dd = String(today.getDate()).padStart(2, '0');
-			let mm = String(today.getMonth() + 1).padStart(2, '0');
-			let yyyy = String(today.getFullYear());
+		let divNewPosition = document.createElement("div");
+		divNewPosition.id = "position1-"+numberOfElements;
+		divNewPosition.classList.add("position");
+		
+		let divContent = document.createElement("div");
+		divContent.innerHTML = taskContent;
+		divContent.classList.add("nongreyed");
 
-			let hh = String(today.getHours()).padStart(2, '0');
-			let ii = String(today.getMinutes()).padStart(2, '0');
-			let ss = String(today.getSeconds()).padStart(2, '0');
+		let divDate = document.createElement("div");
+		divDate.innerHTML = formatCurrentDate();
+		divDate.style.display = "none";
+		//divDate.classList.add('nonvisible');
+
+		//to be rewritten into Jquery
+		//let buttonX = document.createElement("button");
+		//buttonX.innerHTML = 'x';
+
+		divNewPosition.appendChild(divContent);
+		divNewPosition.appendChild(divDate);
+		//divNewPosition.appendChild(buttonX);
+		//$('<button>', {
 			
-			newElement.children[0].innerHTML = dd + '/' + mm + '/' + yyyy + ' ' + hh + ':' + ii + ':' + ss;
-			if (newElement.children[0].style.display === "none") {
-                            newElement.children[0].style.display = "block";
-                        } else {
-                            newElement.children[0].style.display = "none";
-                        }
-		};
-		document.getElementById('list1').appendChild(newElement);		
+		//}).hmtl('x').appendTo(divNewPosition);	
+
+		divContent.onclick = () => {
+			divContent.classList.toggle("nongreyed");
+			divContent.classList.toggle("greyed");
+			divDate.innerHTML = formatCurrentDate();
+			if(divDate.style.display === "none"){
+				divDate.style.display = "block";
+			}
+			else{
+				divDate.style.display = "none";
+			}
+		}
+
+		let nr = numberOfElements;	
+
+		$(divNewPosition).append(
+			$('<button>', {
+			}).html('x').click(() => {
+				//te dwie linijki w jquery!!!
+				//nie kopiują się listenery!!!
+				//trash = divNewPosition.cloneNode(true);
+				//let $copiedElement = $('#position1-'+nr).clone(true);
+				$("#modal").css('display', 'block');
+				//TAK
+				$("#modal").children().eq(1).off("click").click(()=>{
+					$trash = $('#position1-'+nr).clone(true);
+					//divNewPosition.remove();
+					$('#position1-'+nr).remove();
+					$("#modal").css('display', 'none');
+				});
+				$("#modal").children().eq(2).off("click").click(()=>{
+					//$trash = $('#position1-'+nr).clone(true);
+					//divNewPosition.remove();
+					//$('#position1-'+nr).remove();
+					$("#modal").css('display', 'none');
+				});
+
+
+				//$trash = $('#position1-'+nr).clone(true);
+				//divNewPosition.remove();
+				//$('#position1-'+nr).remove();
+			})
+		);
+
+		
+		document.getElementById('list1').appendChild(divNewPosition);	
+		numberOfElements++;
 	}
 }
 
 const przywroc = function(){
-	if(trash!==""){
-		document.getElementById("list1").appendChild(trash);
-		//trash = "";
+	if($trash!==""){
+		//listenery się nie kopuiją
+		//document.getElementById("list1").appendChild($trash);
+		let $copy = $trash.clone(true);
+		//$copy.click(()=>{
+			
+		//});
+		$('#list1').append($copy);
+		$copy.children().eq(0).addClass('nongreyed');
+		$copy.children().eq(0).removeClass('greyed');
+		//$copy.children().eq(1).addClass('nonvisible');
+		$copy.children().eq(1).css('display', 'none');
+		//$copy.children().eq(1).addClass('nonvisible');
+		//$copy.children()
+		$copy.children().eq(0).click(()=>{
+			$copy.children().eq(0).toggleClass('greyed');
+			$copy.children().eq(0).toggleClass('nongreyed');
+			$copy.children().eq(1).html(formatCurrentDate());
+			//$copy.children().eq(1).toggleClass('visible');
+			//console.log($copy.children().eq(1).css('display'));
+			if($copy.children().eq(1).css('display') === 'block'){
+				$copy.children().eq(1).css('display', 'none');
+			}
+			else{
+				$copy.children().eq(1).css('display', 'block');
+			}
+		});
+
+
+		$trash = "";
 	}
+}
+
+function formatCurrentDate(){
+	let today = new Date();
+	let dd = String(today.getDate()).padStart(2, '0');
+	let mm = String(today.getMonth() + 1).padStart(2, '0');
+	let yyyy = String(today.getFullYear());
+
+	let hh = String(today.getHours()).padStart(2, '0');
+	let ii = String(today.getMinutes()).padStart(2, '0');
+	let ss = String(today.getSeconds()).padStart(2, '0');
+	
+	return dd + '/' + mm + '/' + yyyy + ' ' + hh + ':' + ii + ':' + ss;
 }
